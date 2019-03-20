8919a0049ac28dd214e24a316c55240049b79ea8
F-DALLA-VALLE <dallavalle.fabien@gmail.com>
2019-03-20 11:14:27 -0500

    Corrected tests.

--- test/test.js
+++ test/test.js
@@ -1,101 +1,107 @@
 const assert = require('assert');
 const t = require('../index');
 
-const black = '#000000';
+// describe('getBoards', function () {
+//   it('can get boards', function () {
+//     return t.getBoards(apiKey).then((boards) => {
+//       assert.ok(boards);
+//       assert.ok(boards[0]);
+//       assert.ok(boards[0].id);
+//     }).catch((error) => {
+//       assert.fail(error);
+//     })
+//   })
+// });
 
-describe('MiniMeter', function () {
-  describe('#getMetric', function () {
-    it('#getMetric(default)', async function () {
-      return buildApp().then(app => app.getMetric(t.ModeEnum.CPU).then(percent => {
-        console.log("CPU Usage is: " + percent);
-        assert.ok(percent);
-      }))
+// describe('getActionsForBoard', function () {
+//   it('can get actions for board', function () {
+//     return t.getActionsForBoard('5be9f7545b05e45e0092f4dd',
+//       apiKey).then(actions => {
+//       assert.ok(actions);
+//       assert.ok(actions[0]);
+//       assert.ok(actions[0].id);
+//     }).catch(error => {
+//       assert.fail(error);
+//     })
+//   })
+// });
+
+describe('MojoHelpdesk', () => {
+  async function makeApp() {
+    let app = new t.MojoHelpdesk();
+
+    await app.processConfig({
+      extensionId: 777,
+      geometry: {
+        width: 1,
+        height: 1,
+      },
+      authorization: {
+        apiKey: "<Your API key>"
+      },
+      config: {
+        color:"#ff0000",
+        effect: "BLINK",
+        firstName: "joe",
+        option: "nut"
+      }
     });
-    it('#getMetric(freemem)', async function () {
-      return buildApp().then(app => app.getMetric(t.ModeEnum.FREE_MEM).then(percent => {
-        console.log("Free Mem is: " + percent);
-        assert.ok(percent);
-      }))
-    });
-    it('#getMetric(average)', async function () {
-      return buildApp().then(app => app.getMetric(t.ModeEnum.AVERAGE_LOAD).then(percent => {
-        console.log("Average Load is: " + percent);
-        // Windows always return 0
-        assert.ok(percent || percent === 0);
-      }))
-    });
-    it('#getMetric(nvidiaGpu)', async function () {
-      return buildApp().then(app => app.getMetric(t.ModeEnum.NVIDIA_GPU).then(percent => {
-        console.log("nVidia GPU is: " + percent);
-        // Does not work unless nVidia GPU is present?
-        assert.ok(percent || percent === 0);
-      }).catch(error => {
-        console.error("Is an nVidia GPU present?");
-      }))
-    });
+
+    return app;
+  }
+
+  // describe('#getNewActions', () => {
+  //   it("gets new actions with old timestamp", async function () {
+  //     return makeApp().then(async app => {
+  //       app.timestamp = new Date('2018-01-17T03:24:00').toISOString();
+  //       return app.getNewActions().then(actions => {
+  //         assert.ok(actions);
+  //         assert.ok(actions[0]);
+  //         assert.ok(actions[0].id);
+  //       }).catch(error => {
+  //         assert.fail(error);
+  //       })
+  //     })
+  //   });
+
+  //   it("doesn't return new actions with current timestamp", async function () {
+  //     return makeApp().then(async app => {
+  //       return app.getNewActions().then(actions => {
+  //         assert.ok(actions);
+  //         assert(actions.length === 0);
+  //       }).catch(error => {
+  //         assert.fail(error);
+  //       })
+  //     })
+  //   });
+  // });
+
+  // describe('#generateSignal()', () => {
+  //   it('generates a signal', async function () {
+  //     return makeApp().then(async app => {
+  //       const actions = require('./test-actions.json');
+  //       const signal = app.generateSignal(actions);
+  //       console.log(signal);
+  //       assert.ok(signal);
+  //       assert(signal.message.includes(`You have ${actions.length}`));
+  //       assert(signal.link.url.includes('https://trello.com'));
+  //       assert(signal.link.label.includes('Check'));
+  //     }).catch((error) => {
+  //       assert.fail(error)
+  //     });
+  //   })
+  // })
+
+  describe('#run()', () => {
+    it('runs', async function () {
+      return makeApp().then(async app => {
+        // app.timestamp = new Date('2018-01-17T03:24:00').toISOString();
+        return app.run().then((signal) => {
+          assert.ok(signal);
+        }).catch((error) => {
+          assert.fail(error)
+        });
+      })
+    })
   })
-
-  it('#getColor', async function () {
-    return buildApp().then(app => {
-      assert.equal(black, app.getColor(99), 'Expected big number to return black');
-      assert.equal(black, app.getColor(-1), 'Expected negative number to return black');
-
-      assert(app.getColor(0), 'Expected 0 to return a color');
-      assert.notEqual(black, app.getColor(0), 'Expected 0 to return a color');
-
-      assert(app.getColor(.999), 'Expected .999 to return a color');
-      assert.notEqual(black, app.getColor(.999), 'Expected .999 to return a color');
-    })
-  });
-
-  it('#generatePoints', async function () {
-    return buildApp().then(app => {
-      for (let i = 0; i < 1; i += 0.01) {
-        const points = app.generatePoints(i);
-        assert.equal(1, points.length)
-        assert.notEqual('black', points[0].color);
-        assert(points[0].color.match(/^#[A-Fa-f0-9]{6}$/));
-      }
-    })
-  });
-
-  it('#run()', async function () {
-    return buildApp().then(async app => {
-      try {
-        const signal = await app.run();
-        console.log(JSON.stringify(signal));
-        assert.ok(signal);
-        assert.ok(signal.points[0][0], "I should have at least one point");
-      }
-      catch (error) {
-        assert.fail(error);
-      }
-    })
-  });
-
-  it('deletes old signals', function () {
-    return buildApp().then(app => {
-      return app.deleteOldSignals();
-    })
-  })
-});
-
-async function buildApp(config) {
-  config = config || {};
-  let app = new t.MiniMeter();
-  app.config = {
-    mode: t.ModeEnum.CPU,
-    geometry: {
-      height: 1,
-      width: 1,
-      origin: {
-        x: 1,
-        y: 1
-      }
-    },
-
-    ...config
-  };
-
-  return app;
-}
+})
